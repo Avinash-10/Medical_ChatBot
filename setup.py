811f925eecc5e4724db63258820ce0cb63865e13
Avinash Vyas <avinash1999.vyas@gmail.com>
2024-01-07 02:27:37 +0530

    code updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Medical ChatBot",
+    version = "0.0.0",
+    author = "Avinash Vyas",
+    author_email = "avinash1999.vyas@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
